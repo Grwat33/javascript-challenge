32b348471986ae360c46f342754108cedb806fb9
Grwat33 <Grwat33@gmail.com>
2020-12-10 17:22:38 -0500

    imported table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach(function(UFO) {
+    console.log(UFO);
+    var row = tbody.append("tr");
+    Object.entries(UFO).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
